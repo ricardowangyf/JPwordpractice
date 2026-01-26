a284ba253c5e3428dd38dca3e612922326bec581
Ricardowang <1758555877@qq.com>
2026-01-26 22:38:36 +0800

    .

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-
 module.exports = {
+  // 打包后的静态资源引用路径
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/JPwordpractice/'  // 仓库名
-    : '/'
+    ? '/JPwordpractice/' // 你的 GitHub 仓库名
+    : '/',
+
+  outputDir: 'dist', // 构建输出目录
 }
