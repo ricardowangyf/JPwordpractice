91264e21d3cdb2588f6cb0e9f564f952fca3e527
Ricardowang <1758555877@qq.com>
2026-01-27 22:01:44 +0800

    daily jp study log 2026-01-27

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
+// Vue CLI
 module.exports = {
-  // 打包后的静态资源引用路径
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/JPwordpractice/' // 你的 GitHub 仓库名
-    : '/',
-
-  outputDir: 'dist', // 构建输出目录
+  publicPath: process.env.NODE_ENV === 'production' ? '/JPwordpractice/' : '/'
 }
+
+// Vite
+export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/JPwordpractice/' : '/'
+})
