c7c37c5af285742c01a2f19a7168afe53d5a250e
Ricardowang <1758555877@qq.com>
2026-01-29 23:08:24 +0800

    daily jp study log 2026-01-29

--- src/data/jpTrainer.js
+++ src/data/jpTrainer.js
@@ -1,23 +1,44 @@
 export default [
-    {
-      id: 1,
-      type: 'word',
-      question: '勉強（べんきょう）',
-      answer: '学习',
-    
-    },
-    {
-      id: 1,
-      type: 'word',
-      question: '休みです',
-      answer: '休息',
-    
-    },
-    {
-      id: 2,
-      type: 'sentence',
-      question: '何のために日本に来ましたか。',
-      answer: '日本で語学学校に通うためです。'
-    }
-  ]
-  
+  {
+    id: 1,
+    type: 'word',
+    question: '勉強（べんきょう）',
+    answer: '学习',
+  },
+  {
+    id: 2,
+    type: 'word',
+    question: '休みです',
+    answer: '休息',
+  },
+  {
+    id: 3,
+    type: 'word',
+    question: 'ふね',
+    answer: '船',
+  },
+  {
+    id: 4,
+    type: 'word',
+    question: 'えき',
+    answer: '车站',
+  },
+  {
+    id: 5,
+    type: 'word',
+    question: 'かのじよ',
+    answer: '女朋友',
+  },
+  {
+    id: 6,
+    type: 'word',
+    question: 'ひど',
+    answer: '人',
+  },
+  // {
+  //   id: 4,
+  //   type: 'sentence',
+  //   question: '何のために日本に来ましたか。',
+  //   answer: '日本で語学学校に通うためです。'
+  // }
+]
